f168820aa1d17f94852813a20ec95940d2579c66
Tyler Mau <tmau@calpoly.edu>
2015-01-12 20:51:03 -0800

    Created Blank Functions

--- trace.c
+++ trace.c
@@ -5,4 +5,28 @@
  * Date: January 12, 2015
  */
 
+int main(int argc, char *args) {
+   return 0;
+}
+
+static void ethernet() {
+
+}
+
+static void ip() {
+
+}
+
+static void tcp() {
+
+}
+
+static void arp() {
+
+}
+
+static void icmp() {
+
+}
+
 
