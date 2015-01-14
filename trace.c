dd8986b4d7fac8977b06448db2cb6db6328fae6e
Tyler Mau <tmau@calpoly.edu>
2015-01-13 21:42:46 -0800

    Added More Functionality

--- trace.c
+++ trace.c
@@ -1,4 +1,4 @@
-/* Computer Networks: Program 1
+/* Computer Networks: Program 2
  * Packet Trace
  *
  * Author: Tyler Mau
@@ -33,13 +33,14 @@ void ethernet(const u_char *data) {
    struct ethernet_header *eheader;
    eheader = (struct ethernet_header *)data;
 
-   printf("\nEthernet Header Output:\n\n");
-   printf("Dest MAC: %s\n", ether_ntoa((struct ether_addr *)eheader->dest));
-   printf("Source MAC: %s\n", ether_ntoa((struct ether_addr *)eheader->src));
-   printf("Type: ");
+   printf("\n   Ethernet Header Output:\n\n");
+   printf("      Dest MAC: %s\n", ether_ntoa((struct ether_addr *)eheader->dest));
+   printf("      Source MAC: %s\n", ether_ntoa((struct ether_addr *)eheader->src));
+   printf("      Type: ");
 
    if (eheader->type == ARP) {
       printf("ARP\n");
+      arp(data);
    }
    else if (eheader->type == IP) {
       printf("IP\n");
@@ -51,19 +52,106 @@ void ethernet(const u_char *data) {
 }
 
 void ip(const u_char *data) {
+   struct ip_header *ip;
+   ip = (struct ip_header *)(data + ETHERNET_SIZE);
+   char check[10];
+   int opt;
 
+   printf("\n   IP Header\n");
+   printf("      TOS: 0x%x\n", ip->tos);
+   printf("      TTL: %d\n", ip->ttl);
+   printf("      Protocol: %s\n", protocolType(ip->protocol));
+
+   if (!in_cksum((unsigned short *)ip, sizeof(struct ip_header))) {
+      strcpy(check, "Correct");
+   }
+   else {
+      strcpy(check, "Incorrect");
+   }
+
+   printf("      Checksum: %s (0x%x)\n", check, ntohs(ip->checksum));
+
+   printf("      Sender IP: %s\n", inet_ntoa(ip->src));
+   printf("      Dest IP: %s\n", inet_ntoa(ip->dest));
+
+   opt = ((ip->vers & MASK) << 2) - sizeof(struct ip_header);
+
+   if (ip->protocol == TCP) {
+   }
+   else if (ip->protocol == UDP) {
+      udp(data, opt);
+   }
+   else if (ip->protocol == ICMP) {
+      icmp(data, opt);
+   }
+}
+
+char *protocolType(u_char protocol) {
+   if (protocol == TCP) {
+      return "TCP";
+   }
+   else if (protocol == UDP) {
+      return "UDP";
+   }
+   else if (protocol == ICMP) {
+      return "ICMP";
+   }
+   else {
+      return "Unknown";
+   }
 }
 
 void tcp() {
 
 }
 
-void arp() {
+void udp(const u_char *data, int opt) {
+   struct udp_header *udp;
+   udp = (struct udp_header *)(data + ETHERNET_SIZE + sizeof(struct ip_header) + opt);
 
+   printf("   \nUDP Header\n");
+   printf("      Source Port: %d\n", ntohs(udp->src));
+   printf("      Dest Port: %d\n", ntohs(udp->dest));
 }
 
-void icmp() {
+void arp(const u_char *data) {
+   struct arp_header *arp;
+   arp = (struct arp_header *)(data + ETHERNET_SIZE);
 
+   printf("\n   ARP header\n");
+   printf("      Opcode: ");
+
+   if (arp->opcode == REQUEST) {
+      printf("Request\n");
+   }
+   else if (arp->opcode == REPLY) {
+      printf("Reply\n");
+   }
+   else {
+      printf("Unknown\n");
+   }
+
+   printf("      Sender MAC: %s\n", ether_ntoa((struct ether_addr *)arp->h_srcAddr));
+   printf("      Sender IP: %s\n", inet_ntoa(arp->p_srcAddr));
+   printf("      Target MAC: %s\n", ether_ntoa((struct ether_addr *)arp->h_destAddr));
+   printf("      Target IP: %s\n", inet_ntoa(arp->p_destAddr));
 }
 
+void icmp(const u_char *data, int opt) {
+   struct icmp_header *icmp;
+   icmp = (struct icmp_header *)(data + ETHERNET_SIZE + sizeof(struct ip_header) + opt);
+
+   printf("   \nICMP Header\n");
+   printf("      Type: ");
+
+   if (icmp->type == ICMP_REQUEST) {
+      printf("Request\n");
+   }
+   else if (icmp->type == ICMP_REPLY) {
+      printf("Reply\n");
+   }
+   else {
+      printf("Unknown\n");
+   }
+}
 
--- trace.h
+++ trace.h
@@ -4,18 +4,75 @@
 #include <pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
+#include "checksum.h"
+
+#define ETHERNET_SIZE 14
 #define MAC_LEN 6
 #define ARP 1544
 #define IP 8
+#define TCP 6
+#define UDP 17
+#define ICMP 1
+#define REQUEST 256
+#define REPLY 512
+#define ICMP_REQUEST 8
+#define ICMP_REPLY 0
+#define MASK 0x0F
 
 struct ethernet_header {
 	char dest[MAC_LEN];
 	char src[MAC_LEN];
 	uint16_t type;
-};
+}__attribute__((packed));
+
+struct arp_header {
+	uint16_t hardware;
+	uint16_t protocol;
+	u_char h_len;
+	u_char p_len;
+	uint16_t opcode;
+	u_char h_srcAddr[MAC_LEN];
+	struct in_addr p_srcAddr;
+	u_char h_destAddr[MAC_LEN];
+	struct in_addr p_destAddr;
+}__attribute__((packed));
+
+struct ip_header {
+	u_char vers;
+	u_char tos;
+	uint16_t len;
+	uint16_t id;
+	uint16_t flags;
+	u_char ttl;
+	u_char protocol;
+	uint16_t checksum;
+	struct in_addr src;
+	struct in_addr dest;
+}__attribute__((packed));
+
+struct udp_header {
+	uint16_t src;
+	uint16_t dest;
+	uint16_t len;
+	uint16_t checksum;
+}__attribute__((packed));
+
+struct icmp_header {
+	u_char type;
+	u_char opcode;
+	u_char checksum;
+}__attribute__((packed));
 
 void ethernet(const u_char *data);
+void arp(const u_char *data);
 void ip(const u_char *data);
+void udp(const u_char *data, int opt);
+void icmp(const u_char *data, int opt);
+char *protocolType(u_char protocol);
